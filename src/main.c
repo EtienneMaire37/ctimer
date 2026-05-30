0f2f32f8d1f87e15296d319df1903ee7634caab8
EtienneMaire37 <etiennemaire629@gmail.com>
2026-05-30 17:12:36 +0200

    feat: add basic window drawing

--- src/main.c
+++ src/main.c
@@ -4,15 +4,30 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
+void sigwinch()
+{
+    border(0, 0, 0, 0, 0, 0, 0, 0);
+    refresh();
+}
+
 int main()
 {
     atexit((void*)endwin);
     for (int i = 0; i < SIGRTMIN; i++)
         signal(i, exit);
+    signal(SIGWINCH, sigwinch);
 
-    initscr();
+    WINDOW* scr = initscr();
+    noecho();
+    cbreak();
+    curs_set(0);
+    // * do NOT use resizeterm as it modifies the SIGWINCH handler
+    resize_term(15, 2 * 12);
 
-    while (true);
+    border(0, 0, 0, 0, 0, 0, 0, 0);
+    refresh();
+    while (true)
+        ;
 
     endwin();
 }
